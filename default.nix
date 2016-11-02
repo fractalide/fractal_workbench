c280ba86e4f2f4444f605b999764f5881b85c4ef
Stewart Mackenzie <setori88@gmail.com>
2016-11-02 17:50:47 +0800

    Problem: comments about usage needed and no encrypt component args
    Solution: add them

--- default.nix
+++ default.nix
@@ -2,17 +2,28 @@
   , pkgs ? fractalide.pkgs
   , support ? fractalide.support
   , contracts ? fractalide.contracts
-  , components ? fractalide.components}:
+  , components ? fractalide.components
+  , fractalide_user ? null
+  , config_file ? null}:
 let
-  publicComponentOrSubnet = allComponents.workbench_nand;
-  exeSubnet = allComponents.workbench_test;
+  ### ------------------------------
+  plugMeIntoFractalideUpstream = allComponents.workbench_nand;
+  # ^^^ this component is the top-level component you want to share with the rest of the community.
+  # typically this component will only expose virtual subnet ports.
+  runMeFromThisFrac = allComponents.workbench_test;
+  # ^^^ this component typically contains your test IIPs, not really for sharing
+  ### you shouldn't need to change anything else!
+  ### ------------------------------
+  encrypt = support.encryptComponent {fractalide_user = fractalide_user; keybase_config_file = config_file;};
   allContracts = contracts // import ./contracts {inherit pkgs support allContracts;};
-  allComponents = components // import ./components {inherit pkgs support allContracts allComponents;};
+  allComponents = components
+                  // import ./components {inherit pkgs support allContracts allComponents;}
+                  // {encrypt = encrypt;};
   result = if fractalide == null
-    then publicComponentOrSubnet
+    then plugMeIntoFractalideUpstream
     else import (<fractalide> + "/support/vm/") {inherit pkgs support;
       contracts = allContracts;
       components = allComponents;
-      exeSubnet = exeSubnet;};
+      exeSubnet = runMeFromThisFrac;};
 in
   result
