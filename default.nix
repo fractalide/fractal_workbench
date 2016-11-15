6d94f3ba2de6baffb91d4088366f34e3db43e4dd
Stewart Mackenzie <setori88@gmail.com>
2016-11-15 23:49:41 +0800

    Problem: fvm compile time takes too long
    Solution: write a file and pass in an argument
    instead of having to recompile fvm each time.

--- default.nix
+++ default.nix
@@ -4,21 +4,26 @@
   , support ? fractalide.support
   , contracts ? fractalide.contracts
   , components ? fractalide.components
+  , subnet ? "test" #<--- change only me
 }:
 let
-  exeSubnet = fractalComponents.workbench;
+  isValidSubnet = (builtins.head (pkgs.lib.attrVals [subnet] fractalComponents));
+  defaultSubnet = if (builtins.isAttrs isValidSubnet) then isValidSubnet else null;
   fractalContracts = import ./contracts {inherit pkgs support allContracts;};
   fractalComponents = import ./components {inherit pkgs support allContracts allComponents;};
   allContracts = contracts // fractalContracts;
   allComponents = components // fractalComponents;
-
   plugMeIntoFractalide = { components = fractalComponents; contracts = fractalContracts; };
-  runMeFromFractal = import (<fractalide> + "/support/vm/") {inherit pkgs support;
-    contracts = allContracts;
-    components = allComponents;
-    exeSubnet = exeSubnet;};
+  fvm = import (<fractalide> + "/support/fvm/") {inherit pkgs support;
+    contracts = contracts;
+    components = components;
+  };
   result = if fractalide == null
     then plugMeIntoFractalide
-    else runMeFromFractal;
+    else pkgs.writeTextFile {
+      name = defaultSubnet.name;
+      text = "${fvm}/bin/fvm ${defaultSubnet}";
+      executable = true;
+    };
 in
 result
