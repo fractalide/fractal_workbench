5f19b4170a6bfe7ebfb34dd4588c8e867ca174d9
Stewart Mackenzie <setori88@gmail.com>
2016-11-21 01:56:59 +0800

    Problem: old fractal_*/default.nix not generic enough
    Solution: it's copy paste worthy now

--- default.nix
+++ default.nix
@@ -4,26 +4,22 @@
   , support ? fractalide.support
   , contracts ? fractalide.contracts
   , components ? fractalide.components
-  , subnet ? "test" #<- this is your test component, only change this line
+  , subnet ? "test"
 }:
 let
-  isValidSubnet = (builtins.head (pkgs.lib.attrVals [subnet] fractalComponents));
-  defaultSubnet = if (builtins.isAttrs isValidSubnet) then isValidSubnet else null;
+  defaultSubnet = (builtins.head (pkgs.lib.attrVals [subnet] fractalComponents));
   fractalContracts = import ./contracts {inherit pkgs support allContracts;};
   fractalComponents = import ./components {inherit pkgs support allContracts allComponents;};
   allContracts = contracts // fractalContracts;
   allComponents = components // fractalComponents;
-  plugMeIntoFractalide = { components = fractalComponents; contracts = fractalContracts; service = ./service.nix;};
   fvm = import (<fractalide> + "/support/fvm/") {inherit pkgs support;
     contracts = contracts;
     components = components;
   };
-  result = if fractalide == null
-    then plugMeIntoFractalide
-    else pkgs.writeTextFile {
-      name = defaultSubnet.name;
-      text = "${fvm}/bin/fvm ${defaultSubnet}";
-      executable = true;
-    };
+  test = pkgs.writeTextFile {
+    name = defaultSubnet.name;
+    text = "${fvm}/bin/fvm ${defaultSubnet}";
+    executable = true;
+  };
 in
-result
+{ components = fractalComponents; contracts = fractalContracts; test = test; service = ./service.nix; }
