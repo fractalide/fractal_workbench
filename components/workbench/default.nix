2ea1b5f2e3158c24f7fd190f4886783c5783b67e
Stewart Mackenzie <setori88@gmail.com>
2016-11-21 02:39:52 +0800

    Problem: namespace incorrect
    Solution: correct it, still not an entirely good solution though

--- components/workbench/default.nix
+++ components/workbench/default.nix
@@ -9,10 +9,10 @@ buildFractalideSubnet rec {
    subnet = ''
    listen => listen http(${net_http_components.http})
    db_path => input clone(${ip_clone})
-   clone() clone[1] -> db_path get(${app_todo_components.get})
-   clone() clone[2] -> db_path post(${app_todo_components.post})
-   clone() clone[3] -> db_path delete(${app_todo_components.delete})
-   clone() clone[4] -> db_path patch(${app_todo_components.patch})
+   clone() clone[1] -> db_path get(${app_todo_components.todo_get})
+   clone() clone[2] -> db_path post(${app_todo_components.todo_post})
+   clone() clone[3] -> db_path delete(${app_todo_components.todo_delete})
+   clone() clone[4] -> db_path patch(${app_todo_components.todo_patch})
 
    http() GET[/todos/.+] -> input get() response -> response http()
    http() POST[/todos/?] -> input post() response -> response http()
