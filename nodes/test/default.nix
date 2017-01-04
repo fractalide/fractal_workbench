8590c912bbc6794529fc462626e3547bf32cba9e
Stewart Mackenzie <setori88@gmail.com>
2017-01-05 00:19:20 +0800

    Problem: Schema in fractals will probably name collide
    Solution: schema must have full qualified class name and dir hierarchy

--- nodes/test/default.nix
+++ nodes/test/default.nix
@@ -2,9 +2,9 @@
 
 subgraph {
   src = ./.;
-  edges = with edges; [ net_http_edges.address path ];
+  edges = with edges; [ net_http_edges.net_http_address fs_path ];
   flowscript = with nodes; with edges; ''
-  '${net_http_edges.address}:(address="127.0.0.1:8000")' -> listen workbench(${workbench})
-  '${path}:(path="${builtins.getEnv "HOME"}/todos.db")' -> db_path workbench()
+  '${net_http_edges.net_http_address}:(address="127.0.0.1:8000")' -> listen workbench(${workbench})
+  '${fs_path}:(path="${builtins.getEnv "HOME"}/todos.db")' -> db_path workbench()
   '';
 }
